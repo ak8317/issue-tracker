24c345c78f51e828bef7993e7e3f908578f4539c
unknown <ankitkumar8317@gmail.com>
2021-01-06 14:21:03 +0530

    added lazy loading"

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useEffect } from 'react';
+import React, { useEffect ,Suspense,lazy} from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,16 +7,21 @@ import {
   Redirect,
   withRouter,
 } from 'react-router-dom';
-import Signup from './components/auth/Signup';
-import Login from './components/auth/Login';
+import Navbar from './components/layout/NavBar';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import store from './store';
 import setAuthToken from './utils/setAuthToken';
 import { loadUser } from './actions/auth';
-import IssueEdit from './components/issue/IssueEdit';
-import Navbar from './components/layout/NavBar';
-import PropTypes from 'prop-types';
-import IssueList from './components/issue/IssueList';
+// const AvatarComponent = lazy(() => import('./AvatarComponent'));
+const Signup =lazy(( )=>import('./components/auth/Signup'));
+const Login=lazy(()=>import('./components/auth/Login'))
+
+const IssueEdit=lazy(()=>import('./components/issue/IssueEdit'))
+// import IssueEdit from './components/issue/IssueEdit';
+
+const IssueList =lazy(()=>import('./components/issue/IssueList'))
+// import IssueList from './components/issue/IssueList';
 if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
@@ -25,14 +30,19 @@ function App({ auth: { isAuthenticated } }) {
   useEffect(() => {
     store.dispatch(loadUser());
   }, []);
+  const renderLoader = () => <div className="loader"></div>;
   return (
     <Router>
       {isAuthenticated && <Navbar />}
       <Switch>
+      <Suspense fallback={renderLoader()}>
+           
+          
         <Route exact path='/signup' component={Signup} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/home' component={withRouter(IssueList)} />
         <Route exact path='/issues/:id' component={IssueEdit} />
+        </Suspense> 
       </Switch>
       <Redirect from='/' to='/signup' />
     </Router>
